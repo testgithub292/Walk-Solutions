8f5b10d571f675b222425bb1e3c62b164b626eb6
testgithub292 <139734979+testgithub292@users.noreply.github.com>
2025-11-17 19:24:41 +0500

    update

--- payment_tab/scriptpayment.js
+++ payment_tab/scriptpayment.js
@@ -16,6 +16,3 @@
   
   //========================================================================
 
-  function sendMail() {
-    window.location.href = "mailto:admin@get8tech.com?subject=Business%20Inquiry%20Regarding%208Tech%20Payment&body=Dear%208Tech%20Team,%0D%0A%0D%0AI%20am%20interested%20in%20learning%20more%20about%20your%20crypto%20payment%20solutions.%20Please%20let%20me%20know%20how%20we%20can%20proceed%20with%20a%20potential%20collaboration.%0D%0A%0D%0ARegards,%0D%0A[Your%20Name]";
-  }
